b170789b7e3075d05c374571d7db58aac9fda3a9
Maksim Palagin <m.palagin91@gmail.com>
2023-01-09 21:34:02 +0700

    complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    
+    return +Math.abs(n)      //absolute value of a number, (+) - convert to Number
+                .toString()  //convert to String
+                .split('')   //convert to Array
+                .reverse()   //reverses the order of the elements in an array
+                .join('');   //convert to String from reverse array
 }
